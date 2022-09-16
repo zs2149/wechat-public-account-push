f7e76891c4b3b3709062abe568b253ca6ed79809
zs2149 <113690465+zs2149@users.noreply.github.com>
2022-09-16 20:50:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ export const config = {
     // 金山每日一句
     CIBA: true,
     // 每日一言
-    oneTalk: false,
+    oneTalk: true,
     // 土味情话(彩虹屁)
     earthyLoveWords: false,
     // 朋友圈文案
@@ -61,7 +61,7 @@ export const config = {
       // 所在城市
       city: "昆明",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '06-30',
+      horoscopeDate: '09-04',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
